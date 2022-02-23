ca11590ba11a71f17e0cfa8ea8f82b5a8070a073
mohamedhamed <mohamedhame2ed@gmail.com>
2022-02-23 12:05:29 -0500

    first

---
+++ ProjectApp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Names,Images
+# Register your models here.
+# admin.site.register(Nemes)
+admin.site.register(Names)
+admin.site.register(Images)
+
+
---
+++ ProjectApp/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+# Create your models here.
+
+from embed_video.fields import EmbedVideoField
+
+
+class Names(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.TextField(max_length=2000)
+    Video = EmbedVideoField(null=True,blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Images(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.TextField(max_length=2000)
+    image = models.ImageField(upload_to='images/', blank=True)
+
+    @classmethod
+    def save_image(self):
+        self.save()
+
+    @classmethod
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def get_image_by_id(cls, image_id):
+        image = cls.objects.get(id=image_id)
+        return image
+
+
+
+    
+    def __str__(self):
+        return self.name
